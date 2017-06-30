da5981ee044a9537c8405caf915b3e55e5bbd068
Barendregt <barendregt@fastmail.com>
2017-06-30 13:19:29 +0200

    update

--- identify_voxel_tuning.py
+++ identify_voxel_tuning.py
@@ -5,11 +5,12 @@ import os,sys,glob
 import numpy as np
 import scipy.stats as stats
 from scipy.signal import fftconvolve
+import scipy.io as sio
 import nibabel as nib
 import pickle
 import tables
 
-# from Staircase import ThreeUpOneDownStaircase
+from Staircase import ThreeUpOneDownStaircase
 from tools import two_gamma as hrf
 from tools import add_subplot_axes
 
@@ -32,7 +33,7 @@ rois = ['V1']#,'V2','MT','BA3a','BA44','BA45']
 # mapper location order (from params):
 # (T=top,B=bottom,L=left,R=right)
 # TR-BR-BL-TL
-exp_location_order = [3, 0, 2, 1]
+# exp_location_order = [3, 0, 2, 1]
 
 
 def bootstrap(data, num_samples, statistic, alpha):
@@ -51,30 +52,50 @@ for subid in subs:
 	data_dir = '/home/shared/2017/visual/OriColorMapper/preproc/'
 	#data_dir = '/home/barendregt/Projects/Attention/'
 	nifti_dir = os.path.join(data_dir, subid, 'psc/')
+	deriv_dir = os.path.join(data_dir, subid, 'deriv/')
 	ROI_dir = os.path.join(data_dir, subid, 'masks/dc/')
 	pickle_dir = os.path.join(data_dir, subid, 'beh/')
 	fig_dir = os.path.join(data_dir, subid, 'figures/')
 
-
+	if not os.path.isdir(deriv_dir):
+		os.makedirs(deriv_dir)
 
 	# Locate nifti files
 	nifti_files = glob.glob('%s*_task-%s_*.nii.gz'%(nifti_dir, task))
-	trialinfo_files = glob.glob('%s*_task-%s_*_trialinfo.pickle'%(nifti_dir, task))
-	params_files = glob.glob('%s*_task-%s_*_params.pickle'%(nifti_dir, task))
+	trialinfo_files = glob.glob('%s*_task-%s_*_trialinfo.pickle'%(pickle_dir, task))
+	params_files = glob.glob('%s*_task-%s_*_params.pickle'%(pickle_dir, task))
 
-
-	embed()
-
-	mri_data = {}
-
-	for ROI in rois:
-		# Get all cortex data and task orders
-		lh_mask = np.array(nib.load(os.path.join(ROI_dir,'lh.%s_vol_dil.nii.gz'%ROI)).get_data(), dtype = bool)
-		rh_mask = np.array(nib.load(os.path.join(ROI_dir,'rh.%s_vol_dil.nii.gz'%ROI)).get_data(), dtype = bool)
-
-		mri_data[ROI] = np.array([np.vstack(nib.load(nf).get_data()[lh_mask,:], nib.load(nf).get_data()[rh_mask,:]) for nf in nifti_files])
+	nifti_files.sort()
+	trialinfo_files.sort()
+	params_files.sort()
 
 	
 
-	trialinfo = []
-	params = []
+	# Load fMRI data if not previously saved
+	if not os.path.isfile(os.path.join(deriv_dir,'roi_data.mat')):
+
+		mri_data = {}
+
+		for ROI in rois:
+			# Get all cortex data and task orders
+			lh_mask = np.array(nib.load(os.path.join(ROI_dir,'lh.%s_vol_dil.nii.gz'%ROI)).get_data(), dtype = bool)
+			rh_mask = np.array(nib.load(os.path.join(ROI_dir,'rh.%s_vol_dil.nii.gz'%ROI)).get_data(), dtype = bool)
+
+			mri_data[ROI] = np.array([np.vstack([nib.load(nf).get_data()[lh_mask,:], nib.load(nf).get_data()[rh_mask,:]]) for nf in nifti_files])
+
+		
+		sio.savemat(file_name=os.path.join(deriv_dir,'roi_data.mat'), mdict=mri_data)
+	else:
+		mri_data = sio.loadmat(os.path.join(deriv_dir,'roi_data.mat'))
+
+
+	task_data = {'trial_order': [],
+				 'trial_stimuli': []}
+
+	for ti,par in zip(trialinfo_files, params_files):
+		[trial_array, trial_indices, trial_params, per_trial_parameters, per_trial_phase_durations, staircase] = pickle.load(open(ti,'rb'))
+
+		task_data['trial_order'].append(trial_params[:,0])
+		task_data['trial_stimuli'].append(trial_array)
+
+	# embed()
